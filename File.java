85c9c8edab61bf93e72cceaf37a0f87b7bb1fe5a
JANANI-SUBASHINI <janani.jananir.r@gmail.com>
2017-07-04 16:41:28 +0530

    Create File.java

---
+++ File.java
@@ -0,0 +1,13 @@
+# Hello
+import java.io.*;
+import java.util.*;
+public class File
+{
+  public static void main(String args[])throws IOException
+  {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    for(int i=0;i<5;i++)
+    System.out.println("Hello");
+    }
+    }
+    
